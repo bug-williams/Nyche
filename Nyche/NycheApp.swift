802e9e9fec252fe0daad9cdbc98bfabde4402f40
Zachary Williams <zachwilliams.services@gmail.com>
2020-06-29 16:08:33 -0400

    Initial Commit

---
+++ Nyche/NycheApp.swift
@@ -0,0 +1,17 @@
+//
+//  NycheApp.swift
+//  Nyche
+//
+//  Created by Zach on 6/29/20.
+//
+
+import SwiftUI
+
+@main
+struct NycheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
